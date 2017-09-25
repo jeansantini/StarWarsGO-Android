4fb89ff6b0811d6699a779afbbfb935aa85e2e63
jsantini <jean.ferreira.santini@gmail.com>
2017-09-25 12:08:04 -0300

    add CaptureQRCodePresenter

--- StarWarsGO/app/src/main/java/com/starwarsgo/ui/captureQRCode/CaptureQRCodePresenter.java
+++ StarWarsGO/app/src/main/java/com/starwarsgo/ui/captureQRCode/CaptureQRCodePresenter.java
@@ -1,8 +1,54 @@
 package com.starwarsgo.ui.captureQRCode;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.starwarsgo.data.source.PersonDataSource;
+import com.starwarsgo.model.Person;
+
 /**
  * Created by jsantini on 25/09/17.
  */
 
-public class CaptureQRCodePresenter {
+public class CaptureQRCodePresenter  implements CaptureQRCodeContract.Presenter {
+
+    private PersonDataSource mPersonDataSource;
+
+    @Nullable
+    private CaptureQRCodeContract.View mQRCodeView;
+
+    CaptureQRCodePresenter(@NonNull final PersonDataSource personDataSource) {
+        mPersonDataSource = personDataSource;
+    }
+
+    @Override
+    public void setView(final CaptureQRCodeContract.View view) {
+        mQRCodeView = view;
+    }
+
+    @Override
+    public void dropView() {
+        mQRCodeView = null;
+    }
+
+    public void savePerson(String url) {
+        Person person = new Person(url);
+        mPersonDataSource.savePerson(person, new PersonDataSource.SavePersonCallback() {
+            @Override
+            public void onPersonSaved() {
+                mQRCodeView.showSuccessSavePerson();
+            }
+
+            @Override
+            public void onPersonAlreadyExists() {
+                mQRCodeView.showErrorPersonAlreadyExists();
+            }
+
+            @Override
+            public void onPersonSaveError() {
+                mQRCodeView.showSavePersonError();
+            }
+        });
+
+    }
 }
